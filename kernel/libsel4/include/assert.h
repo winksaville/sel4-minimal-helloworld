31b29ca4384cce87b386c1dfffa15dd598d012b9
Wink Saville <wink@saville.com>
2015-06-22 15:40:01 -0700

    Remove the dependency on libc from kernel/libsel4.

---
+++ kernel/libsel4/include/assert.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2015, Wink Saville
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ */
+
+#ifndef __ASSERT_H
+#define __ASSERT_H
+
+// For the time being no asserts
+#define assert(x) ((void)0)
+
+#endif
